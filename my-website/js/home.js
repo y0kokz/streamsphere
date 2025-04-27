aa6c48ed0c245634051ffc3ebce471409f31003e
y0kokz <137493671+y0kokz@users.noreply.github.com>
2025-04-28 04:02:08 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'b66dd199385a468c848db1e409b3dc77';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
